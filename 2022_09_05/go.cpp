b1bf7dbff0acc9e50fc269bc874defbdf0df1175
Replit user <>
2022-09-05 04:29:53 +0000

    update

---
+++ 2022_09_05/go.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+double centi_to_meter ( int cm );
+
+int main(void) {
+
+    double res;
+
+    res = centi_to_meter(187);
+    printf("%.2lfM\n", res);
+
+    return 0;
+}
+
+double centi_to_meter ( int cm ) {
+
+    double temp;
+    
+    temp = (double) cm / 100;
+
+    return temp;
+}
---
+++ 2022_09_05/sample1.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int sum ( int x, int y );
+
+int main() {
+    int a = 10, b = 20;
+    int result;
+
+    result = sum( a, b );
+    printf("result : %d\n", result);
+
+    return 0;
+}
+
+int sum( int x, int y ) {
+    int temp;
+    temp = x + y;
+
+    return temp;
+}
