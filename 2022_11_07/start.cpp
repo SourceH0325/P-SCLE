103a65059257ae0b781ce950df3c8f7a7e602fea
SourceH <zizark4040@gmail.com>
2022-11-14 11:25:39 +0900

    Update start.cpp

--- 2022_11_07/start.cpp
+++ 2022_11_07/start.cpp
@@ -5,17 +5,21 @@ void swap (double *pa, double *pb);
 void line_up (double *max, double *mid, double *min);
 
 
-int main(void){
+int main(void) {
+	
 	double max, mid, min;
+	
 	printf("실수값 3개 입력 : ");
 	scanf("%lf%lf%lf", &max, &mid, &min);
+	
 	line_up(&max, &mid, &min);
 	printf("정렬된 값 출력 : %.1lf, %.1lf, %.1lf\n", max, mid, min);
+	
 	return 0;
 } 
 
-void swap (double *pa, double *pb)
-{
+void swap (double *pa, double *pb) {
+
 	double temp;
 	
 	temp = *pa;
@@ -23,9 +27,12 @@ void swap (double *pa, double *pb)
 	*pb = temp;
 }
 
-void line_up(double *max, double *mid, double *min)
-{
+void line_up(double *max, double *mid, double *min) {
+
   if(*max<*mid) swap(max, mid);
   if(*max<*min) swap(max, min);
   if(*mid<*min) swap(mid, min);
 }
+
+// 실수값 3개 입력 : 3.5 2.7 9.5
+// 정렬된 값 출력 : 9.5, 3.5, 2.7
